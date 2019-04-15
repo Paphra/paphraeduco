b9b083c5491259205b8f7ec3b489034b79efa881
Paphra <paphra.me@gmail.com>
2019-04-15 11:34:23 +0300

    Finished the user Profile and the Viewing of other users. Next steps are to do with enabling private messaging of the users to keep in touch with some group members for discussion

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_moment import Moment
+from flask_babel import Babel
 
 from config import Config
 
@@ -16,6 +18,8 @@ login_manager.login_message = 'Please login to view this page'
 
 db = SQLAlchemy()
 migrate = Migrate()
+moment = Moment()
+babel = Babel()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -25,6 +29,8 @@ def create_app(config_class=Config):
     login_manager.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
+    moment.init_app(app)
+    babel.init_app(app)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
@@ -40,6 +46,9 @@ def create_app(config_class=Config):
 
     from app.posts import bp as posts_bp
     app.register_blueprint(posts_bp)
+
+    from app.users import bp as users_bp
+    app.register_blueprint(users_bp)
 
     return app
 
